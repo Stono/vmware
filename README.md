47c39c8828168f4d8c9abe4bd2507ca3670b8156
Karl Stoney <karl@jambr.co.uk>
2015-03-03 20:02:05 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# vmware
+A vmware client for interacting with various vmware components such as vCloud director.
+Doesnt even do anything at the moment, a place holder for an idea.
+
+## License
+Copyright (c) 2015   
+Licensed under the MIT license.
---
+++ test/vcloud.test.js
@@ -0,0 +1,11 @@
+'use strict';
+var VCloud = require('../lib/vmware').VCloud;
+
+describe('vcloud', function() {
+
+  it('Should create an instance of the vcloud client');
+  it('Should login to the web service');
+  it('Should return an error when login credentials are incorrect');
+  it('Should return a list of the supported versions');
+
+});
